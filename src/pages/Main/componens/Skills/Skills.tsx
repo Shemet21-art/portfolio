cfef7b0875cb528e91b181cdcc24a372b0d9605e
unknown <shemetttttttt8@gmail.com>
2024-11-24 22:19:39 +0100

    add block SKILLS

--- src/pages/Main/componens/Skills/Skills.tsx
+++ src/pages/Main/componens/Skills/Skills.tsx
@@ -7,52 +7,75 @@ import logoJs  from "../../../../common/images/js.png"
 import logoTs  from "../../../../common/images/ts.png"
 import logoVs  from "../../../../common/images/vscode.png"
 import logoReact  from "../../../../common/images/react.png"
+import logoBoots from "../../../../common/images/bootstrap.svg"
 
 import './styles.scss'
+import { useState } from "react"
 
 
 function Skills(){ 
+
+    const[isToggled, setIsToggled] = useState(false);
+
+    const toggleHandler = () => {
+        setIsToggled(!isToggled)
+        console.log(isToggled)
+    }
+
     return(
-    <div className="skills ">
+    <div className={`skills ${isToggled? 'dark' : 'light'}`}>
+        <div className="skills__wrapperSwitch">
+        <p className="darkText">White</p>
+        <label className="switch"> 
+        <input type="checkbox" checked={isToggled} onChange={toggleHandler} />
+        <span className="slider"></span>
+      </label>
+      <p className="lightText">Black</p>
+        </div>
+   
         <div className="skills__container">
         <div className=" skills__row">
-        <div className="skills__block"> 
+        <div className={`skills__block ${isToggled?  'light' : 'dark' }`}> 
             <img className="skills__img" src={logoHtml} alt="" /> 
-            <p className="skills__textBlock">HTML</p> 
+            <p className={`skills__textBlock ${isToggled? 'darkText' :'lightText' }`}>HTML</p> 
         </div>
-        <div className="skills__block"> 
+        <div className={`skills__block ${isToggled?  'light' : 'dark' }`}> 
             <img className="skills__img" src={logoCss} alt="" /> 
-            <p className="skills__textBlock">CSS</p>  
+            <p className={`skills__textBlock ${isToggled? 'darkText' :'lightText' }`}>CSS</p>  
         </div>
-        <div className="skills__block"> 
+        <div className={`skills__block ${isToggled?  'light' : 'dark' }`}> 
             <img className="skills__img" src={logoSass} alt="" /> 
-            <p className="skills__textBlock">SASS</p> 
+            <p className={`skills__textBlock ${isToggled? 'darkText' :'lightText' }`}>SASS</p> 
         </div>
-        <div className="skills__block"> 
+        <div className={`skills__block ${isToggled?  'light' : 'dark' }`}> 
             <img className="skills__img" src={logoGit} alt="" />
-            <p className="skills__textBlock">GIT</p>  
+            <p className={`skills__textBlock ${isToggled? 'darkText' :'lightText' }`}>GIT</p>  
+        </div>
+        <div className={`skills__block ${isToggled?  'light' : 'dark' }`}> 
+            <img className="skills__img" src={logoFigma} alt="" />
+            <p className={`skills__textBlock ${isToggled? 'darkText' :'lightText' }`}>FIGMA</p> 
         </div>
         </div>
         <div className="skills__row ">
-        <div className="skills__block"> 
-            <img className="skills__img" src={logoFigma} alt="" />
-            <p className="skills__textBlock">FIGMA</p> 
-        </div>
-        <div className="skills__block "> 
+        <div className={`skills__block ${isToggled?  'light' : 'dark' }`}> 
             <img className="skills__img" src={logoJs} alt="" /> 
-            <p className="skills__textBlock">JS</p> 
+            <p className={`skills__textBlock ${isToggled? 'darkText' :'lightText' }`}>JS</p> 
         </div>
-        <div className="skills__block "> 
+        <div className={`skills__block ${isToggled?  'light' : 'dark' }`}> 
             <img className="skills__img" src={logoTs} alt="" />
-            <p className="skills__textBlock">TS</p>  
+            <p className={`skills__textBlock ${isToggled? 'darkText' :'lightText' }`}>TS</p>  
         </div>
-        <div className="skills__block"> 
+        <div className={`skills__block ${isToggled?  'light' : 'dark' }`}> 
             <img className="skills__img" src={logoVs} alt="" /> 
-            <p className="skills__textBlock">VSCode</p> 
+            <p className={`skills__textBlock ${isToggled? 'darkText' :'lightText' }`}>VSCode</p> 
         </div>
-        <div className="skills__block"> 
+        <div className={`skills__block ${isToggled?  'light' : 'dark' }`}> 
             <img className="skills__img" src={logoReact} alt="" />
-            <p className="skills__textBlock">React</p>  
+            <p className={`skills__textBlock ${isToggled? 'darkText' :'lightText' }`}>React</p>  
+        </div>
+        <div className={`skills__block ${isToggled?  'light' : 'dark' }`}> 
+            <img className="skills__img" src={logoBoots} alt="" />
+            <p className={`skills__textBlock ${isToggled? 'darkText' :'lightText' }`}>Bootstrap</p>  
         </div>
         </div>
         </div>
