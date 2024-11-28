467774d811b2c64109bb258b70b53715ec856975
unknown <shemetttttttt8@gmail.com>
2024-11-28 19:55:43 +0100

    create blok works

--- src/pages/Main/componens/Works/Works.tsx
+++ src/pages/Main/componens/Works/Works.tsx
@@ -1,9 +1,13 @@
+
+import imgGit from "../../../../common/images/gitphoto.png"
 import './styles.scss'
 
 function Works(){
     return(
-        <div>
-        <p>WORKS</p>
+        <div className='Projects'>
+        <h2>Projects</h2>
+        <p> Links to some works in my repository</p>
+        <a href=""><img src={imgGit} alt="" /></a>
     </div>
     )
    
