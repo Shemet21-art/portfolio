834c7687fecd9e882a36a928444d4a7ca3bfb22c
06011-39-03 <06011-39-03@exchange.ad.afpanet>
2024-12-02 13:30:16 +0100

    add Card

--- src/pages/Main/componens/Works/Works.tsx
+++ src/pages/Main/componens/Works/Works.tsx
@@ -4,10 +4,23 @@ import './styles.scss'
 
 function Works(){
     return(
-        <div className='Projects'>
-        <h2>Projects</h2>
-        <p> Links to some works in my repository</p>
-        <a href=""><img src={imgGit} alt="" /></a>
+        <div className='projects'>
+            
+        <h2 className='projects__title'>Projects</h2>
+        <div className='projects__wrapperTitle' >
+        <p  className='projects__subTitle'> Links to some works in my repository</p>
+        <a  className='projects__gitIcon' href="https://github.com/shemetWebDev"><img src={imgGit} alt="" /></a>
+        </div>
+        <div className="projects__cardsWrapper">
+            <div className="projects__card">
+                <img src="" alt="" />
+                <div className="projects__cardContent">
+                    <p>Text Project </p>
+                </div>
+
+            </div>
+
+        </div>
     </div>
     )
    
